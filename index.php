dbbe84a9bd969f41042c605129518b1fc72fce0d
Ian <ian_scott40@hotmail.com>
2014-09-06 00:06:08 +0800

    First file

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Clients</title>
+
+    <link rel="stylesheet" type="text/css" href="styles.css">
+
+</head>
+<body>
+	
+    <header>
+   	
+        <h1 class="logo">Logo</h1>
+        <nav>
+      	    <ul>      		
+                <li><a href="index.php">Home</a></li>
+                <li><a href="downloads">Downloads</a></li>
+                <li><a href="clients">Clients</a></li>
+                <li><a href="Contact">Contact</a></li>
+         	</ul>
+        </nav>
+    </header>
+    <div id="container">
+   	    <aside>
+   	    	<div class="widget">
+   	            <h2>Widget Header</h2>		
+                <div class="inner">
+                	 Widget Contents
+                </div>
+   	    	</div>
+   	    </aside>
+        <h1>Home</h1>
+        <p>Just a Template</p>
+    </div>
+    <footer>
+    	&copy; Ian Scott 2014. All rights reserved.
+    </footer>
+</body>
+</html>
